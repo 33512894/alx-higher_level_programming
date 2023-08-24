fcac4278cb2b96808418220777d749dadddda98c
33512894 <josphatlemayan74@gmail.com>
2023-08-24 14:19:27 -0700

    python loop

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for numb in range(1, 101):
-        elif numb % 3 == 0 and numb % 5 == 0:
+        if numb % 3 == 0 and numb % 5 == 0:
             print("FizzBuzz ", end="")
         elif numb % 3 == 0:
             print("Fizz ", end="")
